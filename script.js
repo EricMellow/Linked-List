596fda786b6fd2fa126c64601ae38a6b44f006c4
Eric Mellow <ericmellow@gmail.com>
2018-02-07 19:21:46 -0700

    Add URL link functionality
    Add disable button functionality
    Make responsive for smaller devices
    Fix bugs

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-// var webTitle = $('.website-title').val();
-// var webURL = $('.website-url').val();
+var webURL = $('.website-url');
 var enterButton = $('.enter-button');
 var readButton = $('.read-button');
 var rightSide = $('.bookmarks');
 var deleteButton = $('.delete-button');
 
-
-
-// webTitle.addEventListener('keyup', );
-// webURL.addEventListener('keyup', urlValidation);
+webURL.on('keyup', disableButton);
 enterButton.on('click', ConstructBookmark);
 $('aside').on('click', '.read-button', markRead);
 $('aside').on('click', '.delete-button', deleteBookmark);
 
+function disableButton() {
+  var newTitle = $('.website-title').val();
+  var newURL = $('.website-url').val();
+  if (newTitle.length > 0 && newURL.length > 0) {
+    enterButton.prop('disabled', false)
+  } else {
+    enterButton.prop('disabled', true)
+  };
+};
 
 function ConstructBookmark() {
   var newTitle = $('.website-title').val();
@@ -22,31 +27,34 @@ function ConstructBookmark() {
   } else if (newURL === "") {
     alert('Please enter a URL')
   } else {
-  rightSide.append(`<article class="bookmarks-article">
-    <h2 class="website-output">${newTitle}</h2>
-    <p class = "url-output" class="url-read"><a href="${newURL}">${newURL}</a></p>
+    rightSide.append(`<article class="bookmarks-article">
+      <h2 class="website-output">${newTitle}</h2>
+      <p class = "url-output" class="url-read"><a class="anchor" href="https://${newURL}">${newURL}</a></p>
       <div>
-      <button class="read-button">Read</button>
+      <button class="read-button read-hov">Read</button>
       <button class="delete-button">Delete</button>
-    </div>
-  </article>`)
-  focusCursor();
-  $('.website-title').val('');
-  $('.website-url').val('');
-};
+      </div>
+      </article>`)
+    focusCursor();
+    $('.website-title').val('');
+    $('.website-url').val('');
+    disableButton();
+  };
 };
 
 function focusCursor() {
   $('.website-title').focus();
-}
+};
 
 function markRead() {
-    $(this).closest('button').toggleClass('read-read');
-    $(this).closest('article').toggleClass('bookmarks-read');
-    $(this).closest('p').toggleClass('url-read');
+  $(this).closest('button').toggleClass('mark-read');
+  $(this).closest('article').toggleClass('bookmarks-read');
+  $(this).closest('article').find('a').toggleClass('url-read');
+  $(this).closest('article').find('.delete-button').toggleClass('delete-read');
+  $(this).closest('article').find('a').toggleClass('anchor');
+  $(this).closest('button').find('.read-button').toggleClass('read-hov');
 };
 
 function deleteBookmark() {
-  // $(this).closest('article').remove();
-  $(this).closest('article').toggle('read-read');
+  $(this).closest('article').toggle('delete-button');
 };
